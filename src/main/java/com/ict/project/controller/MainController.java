1ad48aa6e56ac485cb29611aa036ec2e4b90a440
jiwoo <starjung402@naver.com>
2025-10-29 12:06:07 +0900

    update

--- src/main/java/com/ict/project/controller/MainController.java
+++ src/main/java/com/ict/project/controller/MainController.java
@@ -1,13 +1,11 @@
 package com.ict.project.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.ict.project.common.MapperUtil;
@@ -16,169 +14,120 @@ import com.ict.project.service.TourService;
 import com.ict.project.vo.MakeupAPIVO;
 import com.ict.project.vo.TboardVO;
 
-
 @Controller
-public class MainController
-{
+public class MainController {
+	
 	@Autowired 
 	private TourService tourService;
 	
+	// ✅ 리다이렉트 추가
+	@GetMapping("/LoginForm")
+	public String redirectToLogin() {
+		return "redirect:/membersLoginForm";
+	}
+	
+	@GetMapping("/RegistrationForm")
+	public String redirectToJoin() {
+		return "redirect:/membersJoinForm";
+	}
+	
 	@GetMapping("/showMain")
-	public ModelAndView goMain()
-	{
+	public ModelAndView goMain() {
 		return new ModelAndView("project/mainHomePage");
-		
 	}
 	
 	@GetMapping("/writeComments")
-	public ModelAndView goWriteComments()
-	{
+	public ModelAndView goWriteComments() {
 		return new ModelAndView("project/writeComments");
-		
 	}
 	
 	@GetMapping("/showMap")
-	public ModelAndView goMap()
-	{
+	public ModelAndView goMap() {
 		return new ModelAndView("project/map");
 	}
 	
-	
 	@GetMapping("/showDetail")
-	public ModelAndView goDetail(@ModelAttribute("area") String area)
-	{
+	public ModelAndView goDetail(@ModelAttribute("area") String area) {
 		return new ModelAndView("project/detail");
 	}
-
-	@GetMapping("/LoginForm")
-	public String showLoginForm() {
-		return "project/loginForm";
-	}
 	
-
-	@GetMapping("/RegistrationForm")
-	public String showRegistrationForm() {
-		return "project/registrationForm";
-	}
-	
-	
-
 	@GetMapping("/showthema")
-	public ModelAndView gothema()
-	{
+	public ModelAndView gothema() {
 		return new ModelAndView("project/thema");
 	}
 	
-
 	@GetMapping("/gotitle")
-	public ModelAndView gotitle()
-	{
+	public ModelAndView gotitle() {
 		return new ModelAndView("project/mainHomePage");
 	}
 	
-	
-
-	
-	
-
 	@GetMapping("/gothema")
-	public ModelAndView gotothema()
-	{
+	public ModelAndView gotothema() {
 		return new ModelAndView("project/thema");
 	}
 	
 	@GetMapping("/gofestival")
-	public ModelAndView gofestival()
-	{
+	public ModelAndView gofestival() {
 		return new ModelAndView("project/festival");
 	}
 	
-	
 	@GetMapping("/findidorpassword")
-	public ModelAndView findidorpassword()
-	{
+	public ModelAndView findidorpassword() {
 		return new ModelAndView("project/findidpassword");
 	}
 	
-
 	@GetMapping("/findpassword")
-	public ModelAndView findpassword()
-	{
+	public ModelAndView findpassword() {
 		return new ModelAndView("project/myPage/findpassword");
 	}
 	
-	
-	
 	@GetMapping("/getid")
-	public ModelAndView getid()
-	{
+	public ModelAndView getid() {
 		return new ModelAndView("project/findid");
 	}
 	
-	
-	
-	
-	
-
-	@GetMapping("/membersJoinForm")
-	public ModelAndView domembersJoinForm()
-	{
-		return new ModelAndView("project/registrationForm");
-	}
-	
 	@GetMapping("/thema1")
-	public ModelAndView goThemadetail()
-	{
+	public ModelAndView goThemadetail() {
 		return new ModelAndView("project/thema1");
 	}
 	
 	@GetMapping("/thema2")
-	public ModelAndView goThemadetail1()
-	{
+	public ModelAndView goThemadetail1() {
 		return new ModelAndView("project/thema2");
 	}
 	
-	
 	@GetMapping("/thema3")
-	public ModelAndView goThemadetail2()
-	{
+	public ModelAndView goThemadetail2() {
 		return new ModelAndView("project/thema3");
 	}
 	
 	@GetMapping("/thema4")
-	public ModelAndView goThemadetail3()
-	{
+	public ModelAndView goThemadetail3() {
 		return new ModelAndView("project/thema4");
 	}
 	
 	@GetMapping("/thema5")
-	public ModelAndView goThemadetail4()
-	{
+	public ModelAndView goThemadetail4() {
 		return new ModelAndView("project/thema5");
 	}
 	
 	@GetMapping("/thema6")
-	public ModelAndView goThemadetail5()
-	{
+	public ModelAndView goThemadetail5() {
 		return new ModelAndView("project/thema6");
 	}
 	
 	@GetMapping("/thema7")
-	public ModelAndView goThemadetail6()
-	{
+	public ModelAndView goThemadetail6() {
 		return new ModelAndView("project/thema7");
 	}
 	
-
 	@GetMapping("/showSupport")
-	public ModelAndView goSupport()
-	{
+	public ModelAndView goSupport() {
 		return new ModelAndView("project/support");
 	}
 	
 	@GetMapping("/showMyPage")
-	public ModelAndView goMyPage()
-	{
+	public ModelAndView goMyPage() {
 		return new ModelAndView("project/myPage/MyPage");
 	}
 	
@@ -188,8 +137,7 @@ public class MainController
 	}
 	
 	@GetMapping("/showBookmark")
-	public ModelAndView goBookmark()
-	{
+	public ModelAndView goBookmark() {
 		return new ModelAndView("project/myPage/Bookmark");
 	}
 	
@@ -208,51 +156,29 @@ public class MainController
 		return new ModelAndView("project/myPage/TravelReviewDetail");
 	}
 	
-	
 	@GetMapping("/eventMap")
-	public ModelAndView goEventMap()
-	{
+	public ModelAndView goEventMap() {
 		return new ModelAndView("project/eventForm");
 	}	
 	
 	@GetMapping("/createDB")
-	public ModelAndView goCreateDB()
-	{
+	public ModelAndView goCreateDB() {
 		ModelAndView mv = new ModelAndView();
-		
-		// 서비스 이동여부 결정 필요 
 		RestApiComm r = new RestApiComm();
 		StringBuffer endPoint = new StringBuffer("https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline");
 		String reslist = r.sendRecv(endPoint, "GET");
 		Gson gsonlist = new Gson();
-		
-		// Gson으로 먼저 API 데이터 파싱
 		List<MakeupAPIVO> apiList = gsonlist.fromJson(reslist.toString(), new TypeToken<List<MakeupAPIVO>>(){}.getType());
-		
 		List<TboardVO> list = MapperUtil.convertToTboardList(apiList);
-
-		// 확인
 		for (TboardVO t : list) {
 		    System.out.println(t.getB_title() + " / " + t.getB_content() + " / " + t.getB_img());
 		}		
-		
 		return null;
-		
-		//int result = tourService.goCreateDB(list);
-		
-		//mv.addObject("list", list);		
-		//mv.addObject("resilt", result);		
-		//mv.setViewName("eventForm");	
-		
-		//return mv;
 	}
 
 	@GetMapping("/showList")
-	public ModelAndView eventList()
-	{
+	public ModelAndView eventList() {
 		System.out.println("Controller showList");
 		return new ModelAndView("project/eventForm");
 	}	
-	
-
 }
--- src/main/webapp/WEB-INF/views/project/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/project/loginForm.jsp
@@ -36,11 +36,11 @@ body {
 .title {
 	font-size: 50px;
 	font-weight: 900;
-	display: inline-flex;      /* ●과 글자를 나란히 배치 */
+	display: inline-flex;
 	align-items: center;
-	gap: 10px;                 /* 점과 글자 간격 */
+	gap: 10px;
 	color: var(--ink);
-	text-decoration: none;     /* 밑줄 제거 */
+	text-decoration: none;
 }
 
 /* ● 파란 점 추가 */
@@ -194,17 +194,17 @@ input[type="reset"]:hover { background: #f1f5f9; }
 	<div id="container">
 		<h2>일반 로그인</h2>
 
-		<form action="/membersLoginOK" method="post">
+		<form action="/membersLogin" method="post" autocomplete="off">
 			<fieldset>
 				<legend>로그인</legend>
 				<ul>
 					<li>
 						<label for="m_id">ID</label>
-						<input type="text" id="m_id" name="m_id" required>
+						<input type="text" id="m_id" name="m_id" autocomplete="off" required>
 					</li>
 					<li>
 						<label for="m_pw">PW</label>
-						<input type="password" id="m_pw" name="m_pw" required>
+						<input type="password" id="m_pw" name="m_pw" autocomplete="new-password" required>
 					</li>
 				</ul>
 
