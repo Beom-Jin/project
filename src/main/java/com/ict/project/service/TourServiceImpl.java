8a46fe96146c3fe9830a68e3b87d3f6df202a26d
dmpark7068 <dmpark7068@gmail.com>
2025-10-29 12:45:36 +0900

    1029

--- src/main/java/com/ict/project/service/TourServiceImpl.java
+++ src/main/java/com/ict/project/service/TourServiceImpl.java
@@ -53,6 +53,36 @@ public class TourServiceImpl implements TourService{
 			return null;
 		}		
 	}
+
+	@Override
+	public int getTourInsert(TboardVO vo) {
+		try {
+			return tourDAO.getTourInsert(vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	@Override
+	public TboardVO getTourUpdatePre(String b_idx) {
+		try {
+			return tourDAO.getTourUpdatePre(b_idx);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}		
+	}
+
+	@Override
+	public int getTourUpdate(TboardVO vo) {
+		try {
+			return tourDAO.getTourUpdate(vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
 	
 	
 
